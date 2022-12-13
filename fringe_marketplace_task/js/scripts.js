0cf568734a0409f641e2e5cb6f74d1769f8d4a31
LewisQuinn18 <34375013+LewisQuinn18@users.noreply.github.com>
2022-12-13 23:52:12 +0000

    Add files via upload

--- fringe_marketplace_task/js/scripts.js
+++ fringe_marketplace_task/js/scripts.js
@@ -5,6 +5,25 @@ $(document).ready(function(){
 			});
 		});
 
+// hide and show section function (off-canvas menu, hidden nav bars, etc...)
+function myFunction_gsm(sectionId) {
+  let sec = document.querySelectorAll('.sec');
+  sec.forEach(itm => {
+    if(itm.id !== sectionId) itm.style.display = 'none'
+  })
+  var x = document.getElementById(sectionId);
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+let sec = document.querySelectorAll('.sec');
+sec.forEach(itm => {
+  itm.style.display = 'none'
+})
+
 // 	Accordion
 var acc = document.getElementsByClassName("accordion");
 var i;
